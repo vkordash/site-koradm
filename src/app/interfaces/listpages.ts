3fcc0d4e5c6601e5bec83f2bb8b4fd5325450427
vkordash <vkordash@gmail.com>
2026-01-03 12:01:20 +0200

    photo.src

--- src/app/interfaces/listpages.ts
+++ src/app/interfaces/listpages.ts
@@ -4,7 +4,9 @@ export interface ListPages {
     title : string;
     date  : string;
     text  : string;
-    photo : string;
     v_len : number;
     id_component : number;
+    photo_src : string;
+    photo_alt : string;
+    photo_title : string;    
 }
